abdf2912d9a1e4fa0fb716ea00513c1c5715b7a7
Tetyana8222 <113339141+Tetyana8222@users.noreply.github.com>
2022-12-24 12:22:53 +0000

    Deploying to gh-pages from @ Tetyana8222/comand-project-icecream@bd2e06861ea4826e3b9ea4fe020a7c6f32b6327d 🚀

---
+++ index.9a003c98.js
@@ -0,0 +1,2 @@
+var slideIndex=0;function carousel(){var e,l=document.getElementsByClassName("mySlides");for(e=0;e<l.length;e++)l[e].style.display="none";++slideIndex>l.length&&(slideIndex=1),l[slideIndex-1].style.display="block",setTimeout(carousel,3e3)}carousel();
+//# sourceMappingURL=index.9a003c98.js.map
